9de7c8f6159b6b7632af29ce5bbb744e4fcf7e02
Mohammed Baksh <mbaksh@redev.co.uk>
2023-03-12 01:49:10 +0000

    Moved tracking properties to their own class

--- Movies.Application/Models/Abstractions/MovieRatingBase.cs
+++ Movies.Application/Models/Abstractions/MovieRatingBase.cs
@@ -4,7 +4,7 @@
 /// Base movie rating containg properties shared between the contracts and the
 /// domain models.
 /// </summary>
-public abstract class MovieRatingBase
+public abstract class MovieRatingBase : Trackable
 {
     /// <summary>
     /// Id of the movie that was rated.
@@ -15,14 +15,4 @@ public abstract class MovieRatingBase
     /// Rating. A number between 1 and 5.
     /// </summary>
     public int Rating { get; set; }
-
-    /// <summary>
-    /// Date when the movie was rated.
-    /// </summary>
-    public DateTime CreatedDate { get; set; }
-
-    /// <summary>
-    /// Date when the movie rating was last updated.
-    /// </summary>
-    public DateTime UpdatedDate { get; set; }
 }
---
+++ Movies.Application/Models/Abstractions/Trackable.cs
@@ -0,0 +1,17 @@
+﻿namespace Movies.Application.Models.Abstractions;
+
+/// <summary>
+/// Class used to add tracking properties to a object.
+/// </summary>
+public abstract class Trackable
+{
+    /// <summary>
+    /// Date and time when the movie was added to the database.
+    /// </summary>
+    public DateTime CreatedDate { get; set; }
+
+    /// <summary>
+    /// Date and time when the movie was last updated.
+    /// </summary>
+    public DateTime UpdatedDate { get; set; }
+}
